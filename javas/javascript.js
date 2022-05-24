a335922acae89475dee4f8e3eaa26b43b497c486
DaviAMattos <105284718+DaviAMattos@users.noreply.github.com>
2022-05-24 08:18:01 -0300

    Add files via upload

---
+++ javas/javascript.js
@@ -0,0 +1,50 @@
+// pega os elemontos da li
+var button = document.querySelectorAll(".key li"), 
+    input = document.querySelector(".input-valor"),
+    operador = document.querySelectorAll(".operador");
+    var oi = 10;  
+for(var i = 0; i < button.length; i++){
+    document.onkeypress = function(event){
+        for(var e = 0; e <= 10; e++){
+            if(key === 48 + e) {
+                input.innerHTML += e;
+            }
+        }
+        switch(key){
+            case 1:
+                input.innerHTML += "*";
+                break;
+            case 2:
+                input.innerHTML += "+";
+                break;
+            case 3:
+                input.innerHTML += "-";
+                break;
+            case 4:
+                input.innerHTML += ".";
+                break;
+            case 5:
+                input.innerHTML += "/";
+                break;            
+            case 6:
+            case 7:
+                 var equacao = input.innerHTML;
+                 if (equacao) {
+                     try {
+                         input.innerHTML = eval (equacao);
+                     } catch (e) {
+                         alert ('Erro na Expressão');
+                     }
+                 }
+        
+    
+     break;
+     case 8:
+     case 9:
+        input.innerHTML = "";
+        break;
+        default:
+        break;
+}}
+
+};
